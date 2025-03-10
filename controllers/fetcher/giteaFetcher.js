176a8246ae120977c1842a1c5f9b0de8b6670650
JaviFdez7 <javidelatorre400@gmail.com>
2025-03-10 18:24:05 +0100

    feat: update Gitea fetcher to support pagination

--- controllers/fetcher/giteaFetcher.js
+++ controllers/fetcher/giteaFetcher.js
@@ -5,14 +5,14 @@ const logger = require('governify-commons').getLogger().tag('fetcher-gitea');
 const fetcherUtils = require('./fetcherUtils');
 
 const eventType = "gitea";
-let storedBranches = []
+let storedBranches = [];
 
 const getInfo = async (options) => {
     try {
         let cached = null;
         try {
             cached = await redisManager.getCache(options.from + options.to + options.endpoint);
-            if(cached){
+            if (cached) {
                 logger.info('[CACHED COMPUTE]: Getting information from redis cache in giteaFetcher', '(Length:', cached.length + ')');
                 return cached;
             }
@@ -22,39 +22,44 @@ const getInfo = async (options) => {
 
         const requestConfig = options.token ? { Authorization: options.token } : {};
 
-        if(options.endpointType === 'newBranches') {
-            let page = 1;
-            let pagedBranches = [];
-            let allBranches = [];
-            do {
-                pagedBranches = await fetcherUtils.requestWithHeaders(options.giteaApiBaseUrl + options.endpoint + '?page=' + page + '&limit=50', requestConfig);
-                allBranches = allBranches.concat(pagedBranches);
-                page++;
-            } while(pagedBranches.length === 50);
-            if(storedBranches.length === 0){
-                storedBranches = allBranches.map(branch => branch.name);
+        let allData = [];
+        let page = 1;
+        let pageSize = 50;
+        let pagedData = [];
+
+        do {
+            let url = `${options.giteaApiBaseUrl}${options.endpoint}?page=${page}&limit=${pageSize}`;
+            pagedData = await fetcherUtils.requestWithHeaders(url, requestConfig);
+            allData = allData.concat(pagedData);
+            page++;
+        } while (pagedData.length === pageSize); // Continua hasta que no haya más datos
+
+        if (options.endpointType === 'newBranches') {
+            if (storedBranches.length === 0) {
+                storedBranches = allData.map(branch => branch.name);
                 return [];
             }
-            const newBranches = allBranches.filter(branch => !storedBranches.includes(branch.name));
-            storedBranches = allBranches.map(branch => branch.name);
+            const newBranches = allData.filter(branch => !storedBranches.includes(branch.name));
+            storedBranches = allData.map(branch => branch.name);
             return newBranches;
         }
-        
-        const data = await fetcherUtils.requestWithHeaders(options.giteaApiBaseUrl + options.endpoint, requestConfig);
+
         const filteredData = await fetcherUtils.applyFilters(
-            data,
+            allData,
             options.from,
             options.to,
             options.mustMatch,
             options.endpointType,
-            eventType 
+            eventType
         );
+
         logger.info('Requesting Gitea URL:', options.giteaApiBaseUrl + options.endpoint, '(Length:', filteredData.length + ')');
+
         redisManager.setCache(options.from + options.to + options.endpoint, filteredData);
         return filteredData;
     } catch (err) {
         logger.error('Error fetching data from Gitea:', err);
-        return []; 
+        return [];
     }
 };
 
